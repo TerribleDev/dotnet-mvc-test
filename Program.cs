585438abc65be448f2e9bea7d9fbf793fbdf0931
Tommy Parnell <tommy@terribledev.io>
2021-12-28 17:25:32 -0500

    api only

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-var port = Environment.GetEnvironmentVariable("PORT") ?? "3100";
-app.Run("http://0.0.0.0:" + port);
+
+app.MapGet("/", () =>
+{
+    return "Hello World";
+});
+app.Run();
