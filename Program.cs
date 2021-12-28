2ba7fb6c8840c8e2ddd453e32fa505225e52e4d4
Tommy Parnell <tommy@terribledev.io>
2021-12-28 18:59:19 -0500

    https

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-
+app.UseForwardedHeaders(new ForwardedHeadersOptions() {
+    ForwardedHeaders = ForwardedHeaders.XForwardedProto
+});
+app.UseHttpsRedirection();
 app.MapGet("/", () => "Hello World!");
 var port = Environment.GetEnvironmentVariable("PORT") ?? "3100";
-app.Run("http://0.0.0.0:" + port);
+app.Run("http://*:" + port);
